6de1ccb9ab41016c6406882c6f4aa32360fb41e6
Warren772 <wogreen@buffalo.edu>
2021-11-07 01:18:15 -0400

    some css finished

--- drugs-and-you/src/App.js
+++ drugs-and-you/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import DrugComponent from './components/DrugsComponent.js';
+import Introduction from './components/Introduction.js'
 
 function App() {
     return (
 	    <div>
+	    <Introduction / > 
 	    <DrugComponent />
 	    </div>
 	
