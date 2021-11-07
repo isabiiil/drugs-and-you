bd4ea9d9db16e6a17fd717de7252fbe5cda83852
feili7080 <88912209+FeiLi1996@users.noreply.github.com>
2021-11-07 02:50:04 -0500

    sadda
    das

--- drugs-and-you/src/components/Introduction.js
+++ drugs-and-you/src/components/Introduction.js
@@ -10,12 +10,12 @@ export const Introduction = props => {
 			<img src={Pills} alt="Pills" width="250" height="250"/>
 			<div className='introduction_wrapper'>
 				<ul>
-					<li>
-					<strong>
-						Greetings!
+						<strong>
+							Greetings!
 						</strong>
-					</li>
+					<li>
 					This app will securely and privately check any interactions between the drugs you take and your prexisting health conditions.
+					</li>
 					<li>
 					Some interactions can be extremely dangerous. For instance if you have asthma, it would be ill advised to take Advil.
 					</li> 
@@ -28,7 +28,9 @@ export const Introduction = props => {
 					<li> 
 					Your drug interactions <strong> will not</strong> be stored server-sided and <strong>will not</strong> presist on the UI. 
 					</li> 
+					<li>
 					You can press check interaction button to show possible interactions
+					</li>
 					<li>
 					<strong> Disclaimer: You should always call your doctor or pharmacists about possible interactions or concerns!!!</strong>
 				
---
+++ drugs-and-you/src/components/UserDrugsComponent.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const drug_list=['d','d','d','d','d'];
+const disease_list=['s','s','s','s','s'];
+
+export const UserDrugsComponent = props => {
+    //props.drugs
+    //props.diseases
+    hello = drug_list.map{x,y=> <div key={y}>{x} </div> }
+    hola = disease_list.map{x=> <div>{x} </div> }
+  return (
+    <div>
+        <div>
+            {hello}
+            {hola}
+        </div>
+    </div>
+      );
+    };
